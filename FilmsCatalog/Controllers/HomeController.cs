0d8bf00f80428cc5e8520b9cac6a6c64db653714
Eugene <nexp1e@stud.tsu.ru>
2022-05-16 16:35:29 +0700

    Movie catalogue display and movie adding implemented

--- FilmsCatalog/Controllers/HomeController.cs
+++ FilmsCatalog/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using FilmsCatalog.Models;
+using FilmsCatalog.Models.Movies;
+using FilmsCatalog.Services.Movies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace FilmsCatalog.Controllers
@@ -12,15 +16,44 @@ namespace FilmsCatalog.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMovieService _movieService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMovieService movieService)
         {
             _logger = logger;
+            _movieService = movieService;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await _movieService.GetAllMovies();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Add()
         {
             return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Add(AddMovieViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+            await _movieService.AddMovie(model, userId);
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
