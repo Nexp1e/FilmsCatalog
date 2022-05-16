64b0016a123dda1c14961f9659fc2aaa511a545e
Eugene <nexp1e@stud.tsu.ru>
2022-05-16 14:47:36 +0700

    Movie repository layed added and implemented

---
+++ FilmsCatalog/Repositories/Movies/IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using FilmsCatalog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FilmsCatalog.Repositories.Movies
+{
+    public interface IMovieRepository
+    {
+        public Task<List<Movie>> GetAllMovies();
+        public Task<Movie> GetMovieById(int id);
+        public Task AddMovie(Movie movie);
+        public Task EditMovie(Movie movie);
+    }
+}
---
+++ FilmsCatalog/Repositories/Movies/MovieRepository.cs
@@ -0,0 +1,41 @@
+﻿using FilmsCatalog.Data;
+using FilmsCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmsCatalog.Repositories.Movies
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MovieRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddMovie(Movie movie)
+        {
+            await _db.Movies.AddAsync(movie);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task EditMovie(Movie movie)
+        {
+            _db.Entry(movie).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<List<Movie>> GetAllMovies()
+        {
+            return await _db.Movies.ToListAsync();
+        }
+
+        public async Task<Movie> GetMovieById(int id)
+        {
+            return await _db.Movies.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+    }
+}
